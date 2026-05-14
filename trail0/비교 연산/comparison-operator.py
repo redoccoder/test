ecaf64225a29eb2b16d3e40491d6633422986dcc
seokju48 <113975085+redoccoder@users.noreply.github.com>
2026-05-14 11:36:44 +0900

    [Passed] 비교 연산 (56ms, 16MB)

---
+++ trail0/비교 연산/comparison-operator.py
@@ -0,0 +1,31 @@
+A, B = map(int, input().split())
+
+if(A >= B):
+    print(int(True))
+else:
+    print(int(False))
+
+if(A > B):
+    print(int(True))
+else:
+    print(int(False))
+
+if(B >= A):
+    print(int(True))
+else:
+    print(int(False))
+
+if(B > A):
+    print(int(True))
+else:
+    print(int(False))
+
+if(A == B):
+    print(int(True))
+else:
+    print(int(False))
+
+if(A != B):
+    print(int(True))
+else:
+    print(int(False))
