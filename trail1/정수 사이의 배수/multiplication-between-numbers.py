ef10154b651f91b160d4de9d6711fca1b36528af
seokju48 <113975085+redoccoder@users.noreply.github.com>
2026-06-29 22:06:52 +0900

    [Passed] 정수 사이의 배수 (54ms, 16MB)

---
+++ trail1/정수 사이의 배수/multiplication-between-numbers.py
@@ -0,0 +1,11 @@
+A, B = map(int, input().split())
+
+sum = 0
+cnt = 0
+
+for i in range(A, B + 1):
+    if i % 5 == 0 or i % 7 == 0:
+        sum += i
+        cnt += 1
+avg = sum / cnt
+print(f"{sum} {avg:.1f}", end=" ")
