4ca356e2c6748e2af55732675434918f0359c4c0
seokju48 <113975085+redoccoder@users.noreply.github.com>
2026-07-09 01:12:54 +0900

    [Passed] 2가 3번째로 등장하는 위치 (51ms, 16MB)

--- trail1/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
+++ trail1/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -2,12 +2,10 @@ N = int(input())
 arr = list(map(int, input().split()))
 
 cnt = 0
-cnt2 = 0
 
-for i in arr:
-    cnt += 1
-    if i == 2:
-        cnt2 += 1
-        if cnt2 == 3:
-            print(cnt)
+for i, var in enumerate(arr):
+    if var == 2:
+        cnt += 1
+        if cnt == 3:
+            print(i + 1)
             break
