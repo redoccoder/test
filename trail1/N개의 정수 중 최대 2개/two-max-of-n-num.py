82d64fcf6bf6d80df14dbdcd3611bebac53a69c7
seokju48 <113975085+redoccoder@users.noreply.github.com>
2026-07-11 17:11:40 +0900

    [Passed] N개의 정수 중 최대 2개 (54ms, 16MB)

--- trail1/N개의 정수 중 최대 2개/two-max-of-n-num.py
+++ trail1/N개의 정수 중 최대 2개/two-max-of-n-num.py
@@ -1,14 +1,14 @@
 n = int(input())
 a = list(map(int, input().split()))
 
-first = float('-inf')
-second = float('-inf')
+first = float("-inf")
+second = float("-inf")
 
 for i in a:
-    if i >= first:
+    if i > first:
         second = first
         first = i
-    elif i >= second:
+    elif i > second:
         second = i
 
 print(first, second)
