80e499996d3106a4ff5703d0610432ebe0322c67
seokju48 <113975085+redoccoder@users.noreply.github.com>
2026-07-20 23:46:20 +0900

    [Passed] 배열의 평균 (49ms, 16MB)

---
+++ trail1/배열의 평균/ave-of-array.py
@@ -0,0 +1,18 @@
+lst = []
+
+for i in range(2):
+    arr = list(map(int, input().split()))
+    lst.append(arr)
+    
+for row in lst:
+    avg1 = sum(row) / 4
+    print(f"{avg1:.1f}", end=" ")
+print()
+
+for j in range(4):
+    avg2 = (lst[0][j] + lst[i][j]) / 2
+    print(f"{avg2:.1f}", end=" ")
+print()
+
+total_avg = (sum(lst[0]) + sum(lst[1])) / 8
+print(f"{total_avg:.1f}")
