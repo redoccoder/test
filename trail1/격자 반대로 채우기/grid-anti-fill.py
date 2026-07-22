95ac12ac93079c3634af8a57467944e8cdb50e2e
seokju48 <113975085+redoccoder@users.noreply.github.com>
2026-07-23 00:28:25 +0900

    [Passed] 격자 반대로 채우기 (49ms, 16MB)

---
+++ trail1/격자 반대로 채우기/grid-anti-fill.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+arr_2d = [[0 for _ in range(n)] for _ in range(n)]
+
+num = 1
+
+for j in range(n - 1, -1, -1):
+    if (n - 1- j) % 2 == 0:
+        for i in range(n - 1, -1, -1):
+            arr_2d[i][j] = num
+            num += 1
+    else:
+        for i in range(n):
+            arr_2d[i][j] = num
+            num += 1
+
+for row in arr_2d:
+    for elem in row:
+        print(elem, end=" ")
+    print()
