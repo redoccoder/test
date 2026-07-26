e19110c0e5145ea3ea3678fda3abd0dfca2cb86e
seokju48 <113975085+redoccoder@users.noreply.github.com>
2026-07-26 16:01:53 +0900

    [Passed] 특정 문자의 등장 횟수 (46ms, 16MB)

---
+++ trail1/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -0,0 +1,13 @@
+a = input()
+
+length = len(a)
+cnt1 = 0
+cnt2 = 0
+
+for i in range(length - 1):
+    if a[i:i + 2] == 'ee':
+        cnt1 += 1
+    elif a[i:i + 2] == 'eb':
+        cnt2 += 1
+
+print(cnt1, cnt2)
